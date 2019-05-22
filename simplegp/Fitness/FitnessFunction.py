9f8454ef31d3530eac2c2cdc8cd4fd1919d157e4
Georgios Andreadis <info@gandreadis.com>
2019-05-22 14:41:19 +0200

    Add progress

--- simplegp/Fitness/FitnessFunction.py
+++ simplegp/Fitness/FitnessFunction.py
@@ -15,8 +15,11 @@ class SymbolicRegressionFitness:
         self.evaluations = self.evaluations + 1
 
         output = individual.GetOutput(self.X_train)
+        print(self.y_train.shape, output.shape, individual.GetSubtree())
+        b = np.cov(self.y_train, output)[0, 1] / np.var(output)
+        a = np.mean(self.y_train) - b * np.mean(output)
 
-        mean_squared_error = np.mean(np.square(self.y_train - output))
+        mean_squared_error = np.mean(np.square(self.y_train - (a + b * output)))
         individual.fitness = mean_squared_error
 
         if not self.elite or individual.fitness < self.elite.fitness:
