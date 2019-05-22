7b315d658969fe97358d2b37b846ce51932a4f9a
Georgios Andreadis <info@gandreadis.com>
2019-05-22 14:17:01 +0200

    Reformat code

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ setup(
     author_email='marco.virgolin@gmail.com',
     url='https://github.com/marcovirgolin/SimpleGP',
     packages=[
-		'simplegp',
-    	'simplegp.Evolution',
-    	'simplegp.Fitness',
-    	'simplegp.Nodes',
-    	'simplegp.Selection',
-    	'simplegp.Variation',
+        'simplegp',
+        'simplegp.Evolution',
+        'simplegp.Fitness',
+        'simplegp.Nodes',
+        'simplegp.Selection',
+        'simplegp.Variation',
     ],
     license='The MIT License',
     long_description=open('README.md').read(),
     install_requires=[
-    	"numpy >= 1.16.1",
+        "numpy >= 1.16.1",
     ],
 )
